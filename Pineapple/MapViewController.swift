8c280f34f46cf534232e78b2eda8c538ba8ae78e
Andrew Carvajal <andrewcarvajal1@gmail.com>
2016-02-14 15:00:46 -0500

    Initial commit

---
+++ Pineapple/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  Pineapple
+//
+//  Created by Andrew Carvajal on 2/7/16.
+//  Copyright © 2016 Andrew Carvajal. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
